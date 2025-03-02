0868fee97299b3abf56035f47ad2f7bcb590b5d0
Blendi <bledilici@hotmail.com>
2025-03-02 12:18:28 +0100

    Create init.sh

---
+++ navidrome/scripts/init.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+# shellcheck disable=SC2015
+set -e
+
+# Install packages
+apk add --no-cache bash >/dev/null
+apk add --no-cache curl >/dev/null
+apk add --no-cache ca-certificates >/dev/null
+apk add --no-cache jq >/dev/null
+apk add --no-cache nginx >/dev/null
+
+# Create folder for NGinx
+mkdir -p /run/nginx
+
+# Create folder for scripts
+mkdir -p /etc/cont-init.d
+chmod -R 755 /etc/cont-init.d
