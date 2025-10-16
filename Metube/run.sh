4159b4a7a9d9df347b1e921f2352924469242c70
Blendi <bledilici@hotmail.com>
2025-10-16 21:34:14 +0200

    Update run.sh

--- Metube/run.sh
+++ Metube/run.sh
@@ -3,6 +3,7 @@
 # shellcheck disable=SC2155,SC2016
 echo "Starting MeTube..."
 set -e
+CONFIG_PATH=/data/options.json
 echo "DOWNLOAD_DIR: $DOWNLOAD_DIR"
 # Always reload DB config from options (to ensure up-to-date values after export)
 #export DOWNLOAD_DIR="$(bashio::config 'DOWNLOAD_DIR')"
