a5fc15b93233a6e961e3c863280e2d653fdd15ad
Blendi <bledilici@hotmail.com>
2025-10-16 22:30:42 +0200

    Update run.sh

--- Metube/run.sh
+++ Metube/run.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/with-contenv bashio
 # shellcheck shell=bash
 # shellcheck disable=SC2155,SC2016
-echo "Starting MeTube..."
+bashio::log.info "Starting MeTube..."
 set -e
 CONFIG_PATH=/data/options.json
 echo "DOWNLOAD_DIR: $DOWNLOAD_DIR"
