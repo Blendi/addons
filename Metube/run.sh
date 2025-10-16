0cb7e7b6b0802dc81e44529edfe05b13cb6c8ce1
Blendi <bledilici@hotmail.com>
2025-10-16 20:22:39 +0200

    Update run.sh

--- Metube/run.sh
+++ Metube/run.sh
@@ -21,7 +21,7 @@ export_options() {
     done
 }
 
-#export_options
+export_options
 # Always reload DB config from options (to ensure up-to-date values after export)
 export DOWNLOAD_DIR="$(bashio::config 'DOWNLOAD_DIR')"
 
