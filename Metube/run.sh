d42646af2a795ca845f4fa945d3c113a75819199
Blendi <bledilici@hotmail.com>
2025-10-17 14:00:31 +0200

    Update run.sh

--- Metube/run.sh
+++ Metube/run.sh
@@ -1,14 +1,19 @@
 #!/bin/ash
 # Use POSIX-compliant script for `sh` shell
 
+# THIS LINE IS CRUCIAL to load the bashio functions
+# It must match where bashio was installed in your Dockerfile.
+. /usr/lib/bashio/bashio.sh 👈 Make sure this path is correct!
+
 set -e
 
 #echo "Fetching DOWNLOAD_DIR from config.yaml..."
-logger "Fetching DOWNLOAD_DIR from config.yaml..."
+#logger "Fetching DOWNLOAD_DIR from config.yaml..."
+bashio::log.info "Fetching DOWN_DIR from add-on configuration..."
 # Fetch value from config.yaml using bashio
-DOWNLOAD_DIR=$(bashio::config 'DOWN_DIR')
+CONFIG_DOWN_DIR=$(bashio::config 'DOWN_DIR')
 
-DOWNLOAD_DIR="${DOWN_DIR:-/media/My_Music/Youtube}"
+DOWNLOAD_DIR="${CONFIG_DOWN_DIR:-/media/My_Music/Youtube}"
 
 
 # Check if DOWNLOAD_DIR is empty
@@ -18,9 +23,10 @@ if [ -z "$DOWNLOAD_DIR" ]; then
 fi
 
 #echo "Using DOWNLOAD_DIR: $DOWNLOAD_DIR"
-logger "Using DOWNLOAD_DIR: $DOWNLOAD_DIR"
+#logger "Using DOWNLOAD_DIR: $DOWNLOAD_DIR"
+bashio::log.info "Using DOWNLOAD_DIR: ${DOWNLOAD_DIR}"
+
 # Export the value to the environment
 export DOWNLOAD_DIR
-
 # Run the Python script
 exec python3 -m metube
